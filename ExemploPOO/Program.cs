487ca539d6dbb74919c844a578f4b4c8f5e4e4dc
Pedro <pedro.vieira02@hotmail.com>
2022-01-22 14:40:14 -0300

    primeiro commit

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program{
+        static void Main(string[] args) 
+        {            
+
+
+            // Retangulo r = new Retangulo();
+
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine("Área : " + r.ObterArea());
+
+
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
+        } 
+    }
+
+}
+
